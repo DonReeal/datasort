af599fcfec0d35d15727d213613b59040f774475
donreeal <gut.bert.gut@googlemail.com>
2019-01-03 00:57:52 +0100

    refactor: Render table headers dynamically

--- src/datasort/core.cljs
+++ src/datasort/core.cljs
@@ -9,46 +9,43 @@
 
 (enable-console-print!)
 
-;; 
-;; 1. user selects sort sort criteria
-;;    => 1.1 build index -> render visible records
-;; 2. select "visualize groups"
-;;    => render visible records in groups; if this becomes a major feature; maybe indexing as group-by would be useful to begin with ...
-;; 
-
-;; ==============================================================================
-(def state (reagent/atom {}))
-
-;; selfmade mini console debugger: traces all changes in state
-(add-watch state :trace-state-changed
-  (fn [key atom old-state new-state]
-    (println (str "== " key " =="))
-    (cljs.pprint/pprint new-state)))
-
 ;; ==============================================================================
 ;; set initial app state
-
 (def dataset
   [{:id 1 :api "POST /foo" :duration 200 :sessionid "xxx-1"}
    {:id 2 :api "POST /foo" :duration 150 :sessionid "xxx-1"}
    {:id 3 :api "POST /bar" :duration 150 :sessionid "xxx-2"}
    {:id 4 :eventid "foo-created" :duration 150 :sessionid "xxx-2"}])
 
-(let [logs (into (sorted-map) 
-                 (map #(vector (:id %) %) dataset))]
-  (swap! state assoc 
-    :logs-by-id logs
-    :sort-criteria [[:id (csort/cmp-fn ::csort/asc)]]))
-    
+(defonce state
+  (reagent/atom 
+    {:columns {0 {:col-id "id"        :resolver-fn :id} 
+               1 {:col-id "api"       :resolver-fn :api} 
+               2 {:col-id "eventid"   :resolver-fn :eventid} 
+               3 {:col-id "duration"  :resolver-fn :duration} 
+               4 {:col-id "sessionid" :resolver-fn :sessionid}}
+     :logs-by-id  (into (sorted-map) (map #(vector (:id %) %) dataset))
+     :sort-criteria [[:id (csort/cmp-fn ::csort/asc)]]}))
+
+(add-watch state :trace-state-changed
+  (fn [key atom old-state new-state]
+    (println (str "== " key " =="))
+    (cljs.pprint/pprint new-state)))
+
+;; ==============================================================================
+;; actions
 (defn update-sort-criteria [new-value]
   (swap! state assoc :sort-criteria new-value))
 
-;; ==============================================================================
-;; actions:
 (defn update-sort-criterion [resolver-kw]
   (println (str "TODO: implement updating sort order by clicking on sort-criterion - " resolver-kw)))
 
-(defn sorted-records []
+;; ==============================================================================
+;; queries
+(defn q-columns []
+  (:columns @state))
+
+(defn q-sorted-records []
   (let [state @state
         index (:logs-by-id state)
         sort-criteria (:sort-criteria state)]
@@ -56,45 +53,41 @@
 
 ;; ==============================================================================
 ;; see https://github.com/reagent-project/reagent-cookbook/blob/master/recipes/sort-table/src/cljs/sort_table/core.cljs
+
 (defn table []
   [:table
     [:thead
       [:tr
-        [:th {:width "200" :on-click #(update-sort-criterion :id)} "id"]
-        [:th {:width "200" :on-click #(update-sort-criterion :api) } "api"]
-        [:th {:width "200" :on-click #(update-sort-criterion :eventid) } "eventid"]
-        [:th {:width "200" :on-click #(update-sort-criterion :duration) } "duration"]
-        [:th {:width "200" :on-click #(update-sort-criterion :sessionid) } "sessionid"]]]
+        (for [[col-index {:keys [col-id resolver-fn]}] (q-columns)]
+          ^{:key col-id}
+          [:th {:width "200" :on-click #(update-sort-criterion resolver-fn)} col-id])]]
     [:tbody
-      (for [record (sorted-records)]
-        ^{:key (:id record)} 
-        [:tr
-          [:td (:id record)]
-          [:td (:api record)]
-          [:td (:eventid record)]
-          [:td (:duration record)] 
-          [:td (:sessionid record)]])]])
-    
+      (let [cols (q-columns)]
+        (for [record (q-sorted-records)]
+          ^{:key (:id record)} 
+          [:tr
+            [:td (:id record)]
+            [:td (:api record)]
+            [:td (:eventid record)]
+            [:td (:duration record)] 
+            [:td (:sessionid record)]]))]])
+
 (defn home []
   [:div {:style {:margin "auto"
                  :padding-top "30px"
                  :width "600px"}}
     [:h1 "Datasort demo"]
-    [:p "sample represents log data - records with arbitrary fields"]
-    [:h4 "Try resorting by using datasort.core/update-sort-criteria"]
-    [:p "E.g. type in your repl:"]
-    [:ul
-      [:li "(in-ns 'datasort.core)"]
-      [:li "(update-sort-criteria [[:sessionid (csort/cmp-fn ::csort/desc)][:duration (csort/cmp-fn ::csort/desc)]])"]
-      [:li "(update-sort-criteria [[:sessionid (csort/cmp-fn ::csort/asc)][:duration (csort/cmp-fn ::csort/desc)]])"]]
+    [:p "Sample uses log data - records with arbitrary fields"]
     [table]])
+    
     
 (defn ^:export main []
   (reagent/render [home] (.getElementById js/document "app")))
 
-  
-
-(update-sort-criteria [[:api (csort/cmp-fn ::csort/asc)]])
-   ;; [:api (csort(cmp-fn ::csort/desc))]])
+(comment
+  ;; Play around resorting the table in your repl:
+  (in-ns 'datasort.core)
+  (update-sort-criteria [[:sessionid (csort/cmp-fn ::csort/desc)][:duration (csort/cmp-fn ::csort/desc)]])
+  (update-sort-criteria [[:sessionid (csort/cmp-fn ::csort/asc)][:duration (csort/cmp-fn ::csort/desc)]]))
    
 
