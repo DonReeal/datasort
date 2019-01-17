afb061ae838f5f831f7ea61521e833e4f2887380
donreeal <gut.bert.gut@googlemail.com>
2019-01-17 01:24:11 +0100

    feature: minimalistic table state model

--- src/cljs/datasort/core.cljs
+++ src/cljs/datasort/core.cljs
@@ -2,44 +2,78 @@
     (:require [cljs.reader]
               [cljs.pprint :as pp]
               [clojure.spec.alpha :as spec]
-              [datasort.criteriasort :as csort]
               [datasort.criteriasort2 :as csort2]
               [reagent.core :as reagent]))
 
 (enable-console-print!)
 
-;; ==============================================================================
-;; app state, init
-
+;; simulating import of some data into our table ui
 (def dataset
   [{:id 1 :api "POST /foo" :duration 200 :sessionid "xxx-1"}
    {:id 2 :api "POST /foo" :duration 150 :sessionid "xxx-1"}
    {:id 3 :api "POST /bar" :duration 150 :sessionid "xxx-2"}
    {:id 4 :eventid "foo-created" :duration 150 :sessionid "xxx-2"}])
 
-(defonce state
-  (reagent/atom 
-    {:logs-by-id (into (sorted-map) (map #(vector (:id %) %) dataset))
-     :columns ;; the columns selected to display
-     [{:col-id "id"        :resolver-fn :id} 
-      {:col-id "api"       :resolver-fn :api} 
-      {:col-id "eventid"   :resolver-fn :eventid} 
-      {:col-id "duration"  :resolver-fn :duration} 
-      {:col-id "sessionid" :resolver-fn :sessionid}]
-     :columns-by-id ;; table model - TODO is associative access required or would a vector do?
-     {"id"        {:resolver-fn :id} 
-      "api"       {:resolver-fn :api} 
-      "eventid"   {:resolver-fn :eventid} 
-      "duration"  {:resolver-fn :duration} 
-      "sessionid" {:resolver-fn :sessionid}}
-     ;; default sort-criteria to be rendered
-     :sort-criteria ;; 
-     [["id"        ::asc]
-      ["api"       ::asc]
-      ["eventid"   ::asc]
-      ["duration"  ::asc]
-      ["sessionid" ::asc]]}))
+(def colspec
+  ;; need a dialogue for the user to specify this
+  ;; minimal version:
+  ;; 1. let user chose ident, or provide default ident by line number (e.g in csv)
+  ;; 2. render ident only
+  [{:col-id "id"        :label "id"        :resolver-fn :id}
+   {:col-id "api"       :label "api"       :resolver-fn :api}
+   {:col-id "eventid"   :label "eventid"   :resolver-fn :eventid}
+   {:col-id "duration"  :label "duration"  :resolver-fn :duration}
+   {:col-id "sessionid" :label "sessionid" :resolver-fn :sessionid}])
 
+
+;; helpers to init table state from dataset
+(defn dataset-by-id [ident-fn dataset]
+  (into (sorted-map) (map #(vector (ident-fn %) %) dataset)))
+;; (dataset-by-id :id dataset)
+
+(defn reduce-keys [records]
+  (into #{} conj (mapcat keys records)))
+(defn reduce-keys2 [records]
+  (reduce clojure.set/union #{} (map #(set (keys %)) records)))
+;; (reduce clojure.set/union #{} (repeat 44000000 #{:a :b :c :d})) ;; => times js out
+;; (into #{} conj (repeat 44000000 '(:a :b :c :d)))                ;; just about works
+
+(defn init-colspec [ident-fn records]
+  (let [first-colspec {:col-id (name ident-fn) :resolver-fn ident-fn :order :asc}
+        otherkeys (disj (reduce-keys records) ident-fn)]
+    (into
+      [first-colspec]
+      (->> otherkeys
+           (map #(hash-map :col-id (name %) :resolver-fn % :order :asc))
+           (sort-by :col-id)))))
+
+;; (init-colspec :id dataset)
+
+
+;; to swap the displayed columns order, change order criteria
+(defn swap [v index1 index2]
+  (assoc v index2 (v index1) index1 (v index2)))
+;; (swap [:id :eventid :bla] 0 2)
+
+
+;; ==============================================================================
+;; table app state, init
+(def denormalized-table-state-model
+  {;; initial columns definition for table row order and sort order definition
+   :colspec ;; (init-colspec :id dataset)
+   [{:col-id "id"        :resolver-fn :id        :order :asc}
+    {:col-id "api"       :resolver-fn :api       :order :asc}
+    {:col-id "duration"  :resolver-fn :duration  :order :asc}
+    {:col-id "eventid"   :resolver-fn :eventid   :order :asc}
+    {:col-id "sessionid" :resolver-fn :sessionid :order :asc}]
+   ;; read index for rendering
+   :logs-by-id ;; (dataset-by-id :id dataset)
+   {1 {:id 1 :api "POST /foo" :duration 200 :sessionid "xxx-1"}
+    2 {:id 2 :api "POST /foo" :duration 150 :sessionid "xxx-1"}
+    3 {:id 3 :api "POST /bar" :duration 150 :sessionid "xxx-2"}
+    4 {:id 4 :eventid "foo-created" :duration 150 :sessionid "xxx-2"}}})
+
+(defonce state (reagent/atom denormalized-table-state-model))
 
 (add-watch state :trace-app-state
   (fn [key atom old-state new-state]
@@ -48,60 +82,37 @@
 
 ;; ==============================================================================
 ;; actions
-(defn update-sort-criteria [new-value]
-  (swap! state assoc :sort-criteria new-value))
 
-(defn toggle-sort-criterion [col-id]
-  @state
-  (let [current-col-order-spec (->> (:sort-criteria @state)
-                                    (filter #(= col-id (first %)))
-                                    (first))]
-    (println current-col-order-spec)
-    (swap! state assoc 
-      :sort-criteria
-      (if (= ::asc (second current-col-order-spec))
-        [[col-id ::desc]]
-        [[col-id ::asc]]))))
+(defn toggle-col-order! [state col-index]
+  (let [colspec0 (:colspec @state)
+        col-colspec0 (nth colspec0 col-index)
+        col-colspec1 (assoc col-colspec0 :order ({:asc :desc :desc :asc} (:order col-colspec0)))
+        colspec1 (assoc colspec0 col-index col-colspec1)]
+    (swap! state assoc :colspec colspec1)))
+
+
+
+
 
 ;; ==============================================================================
 ;; queries
-
-
 ;; ==============================================================================
 
-;; TODO: split in stateful component and render-fn
 
 ;; column-id, column-label, column-index
 
-;; in order selection of the record properties to be displayed in the table
-;; the order specifies the order of the columns in the table to be rendered in the table
-(def colspec [{:col-id "id"        :label "id"        :resolver-fn :id}
-              {:col-id "api"       :label "api"       :resolver-fn :api}
-              {:col-id "eventid"   :label "eventid"   :resolver-fn :eventid}
-              {:col-id "duration"  :label "duration"  :resolver-fn :duration}
-              {:col-id "sessionid" :label "sessionid" :resolver-fn :sessionid}])
-
-;; component local state impl
-
-(defn sort-records [colspec orders records] ;; todo add orders
-  (-> colspec
-      (mapv
-        (fn [{:keys [col-id resolver-fn order]}]
-          ({::asc  {::csort/order ::csort/asc 
-                    ::csort/nils ::csort/last}  
-            ::desc {::csort/order ::csort/desc 
-                    ::csort/nils ::csort/first}}
-           order)))
-      (csort/sort-by-criteria records)))
+;; component local state impl =================================================================
 
 ;; pure render
-(defn render-table [{:keys [colspec records on-toggle-sort]}]
+(defn render-table [{:keys [colspec
+                            records
+                            on-toggle-col-order]}]
   [:table
     [:thead
       [:tr
         (for [[index {:keys [col-id label]}] (map-indexed vector colspec)]
           ^{:key col-id}
-          [:th {:width "200" :on-click #(on-toggle-sort col-id)} label])]]
+          [:th {:width "200" :on-click #(on-toggle-col-order col-id)} label])]]
     [:tbody
       (for [record records]
         ^{:key (:id record)} 
@@ -112,16 +123,23 @@
           [:td (:duration record)] 
           [:td (:sessionid record)]])]])
 
+;; TODO db, vs local state ....
 (defn component-table [records]
   (let [colspec (reagent/atom colspec)
         orders (reagent/atom (into {} (map #(vector (:col-id %) ::asc) @colspec)))]
     [render-table 
-     {:colspec @colspec 
-      :records records ;; todo sort em
-      :on-toggle-sort (fn [col-id]
-                        (let [newval (if (= ::asc (get @orders col-id)) ::desc ::asc)]
-                          (swap! orders assoc col-id newval)         
-                          (println orders)))}]))
+     {:colspec
+      @colspec
+      :records
+      records ;; todo sort em
+      :on-toggle-col-order
+      (fn [col-id]
+        (let [vvv (get @orders col-id)
+              newval ({:asc :desc
+                       :desc :asc} vvv)]
+          (swap! orders assoc col-id newval)
+          (println orders)))}]))
+
 
 (defn home []
   [:div {:style {:margin "auto"
@@ -137,17 +155,6 @@
 
 (comment
   ;; Play around resorting the table in your repl:
-  (in-ns 'datasort.core)
-  
-  ;; sorting by a row ascending
-  (toggle-sort-criterion "id")
-  (toggle-sort-criterion "eventid")
-
-  ;; TODO this does not work atm ...
-  (update-sort-criteria 
-    [["sessionid" [true true]
-      "api"       [true true]]])
-
-  (update-sort-criteria [[:sessionid (csort/cmp-fn ::csort/asc)][:duration (csort/cmp-fn ::csort/desc)]]))
+  (in-ns 'datasort.core))
    
 
