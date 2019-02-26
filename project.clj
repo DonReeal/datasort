31601a0aa89467d3035d3438c4614378597fcb71
DonReeal <gut.bert.gut@googlemail.com>
2019-02-26 23:50:23 +0100

    feature: launch dev server from repl and cursive (#1)
    * feature: launch dev server from repl and cursive#
    
    * Update README.md

--- project.clj
+++ project.clj
@@ -11,8 +11,8 @@
                  [org.clojure/core.async  "0.4.474"]
                  [reagent "0.8.1"]]
 
-  :plugins [[lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
-            ;; [lein-figwheel "0.5.16"]]
+  :plugins [[lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
+            [lein-figwheel "0.5.16"]]
 
   :source-paths ["src/clj" "src/cljs" "src/cljc" "script"]
 
